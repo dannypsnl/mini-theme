824947b383854fd0f302f0304b940dd24670f644
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-01-10 09:18:57 +0800

    init

--- info.rkt
+++ info.rkt
@@ -1,8 +1,25 @@
 #lang info
-(define collection "mini-theme")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/mini-theme.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(dannypsnl))
+(define framework:color-schemes
+  '(#hash((colors
+           .
+           ((framework:syntax-color:scheme:string
+             #(131 154 204))
+            (framework:syntax-color:scheme:constant
+             #(131 154 204))
+            (drracket:real-eval-print-loop:value-color
+             #(131 154 204))
+            (framework:syntax-color:scheme:comment
+             #(160 160 160) italic)
+            (framework:syntax-color:scheme:parenthesis
+             #(200 200 200))
+            (framework:syntax-color:scheme:keyword
+             #(95 85 187))
+            (framework:syntax-color:scheme:hash-colon-keyword
+             #(95 85 187))
+            (framework:syntax-color:scheme:symbol
+             #(28 25 24) bold)
+            (drracket:check-syntax:unused-require
+             #(242 80 65))
+            (framework:syntax-color:scheme:error
+             #(242 80 65))))
+          (name . "Mini"))))
--- scribblings/mini-theme.scrbl
+++ scribblings/mini-theme.scrbl
@@ -1,10 +1,5 @@
 #lang scribble/manual
-@require[@for-label[mini-theme
-                    racket/base]]
-
 @title{mini-theme}
 @author{Lîm Tsú-thuàn}
-
-@defmodule[mini-theme]
 
 Package Description Here
